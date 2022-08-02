b991ea1b803cd6bfe946cc00a4a23e3b40b43bbf
juracucumber1 <mr.gordien@mail.ru>
2022-08-03 01:23:10 +0300

    update
    Favorite func
    Del fav func

--- src/components/RepoCart.tsx
+++ src/components/RepoCart.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IRepo} from "../models/models";
+import {useActions} from "../hooks/actions";
+import {useAppSelector} from "../hooks/redux";
 
 export function RepoCart({ repo }: { repo: IRepo }) {
+
+    const {addFavorite, removeFavorite} = useActions()
+    const {favorites} = useAppSelector(state => state.github)
+
+    const [isFav, setIsFav] = useState(favorites.includes(repo.html_url))
+
+    const addToFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        addFavorite(repo.html_url)
+        setIsFav(true)
+    }
+
+
+    const removeFromFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        removeFavorite(repo.html_url)
+        setIsFav(false)
+    }
+
     return (
 <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-blue-200 transition-all">
     <a href={repo.html_url} target="_blank">
@@ -11,6 +32,17 @@ export function RepoCart({ repo }: { repo: IRepo }) {
             Watchers: <span className="text-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{repo?.description}</p>
+
+
+        { !isFav &&  <button
+            onClick={addToFavorite}
+            className="py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all mr-2"
+        >Add</button>}
+
+        { isFav && <button
+            onClick={removeFromFavorite}
+            className="py-2 px-4 bg-red-600 rounded hover:shadow-md transition-all"
+        >Remove</button>}
     </a>
 </div>
     );
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {useAppSelector} from "../hooks/redux";
 
 export function FavoritesPage() {
+    const {favorites} = useAppSelector(state => state.github)
+
+    if (favorites.length === 0) return <p className="text-center bg-red-600">У вас нет Добавленых репов...</p>
+
     return (
-        <div>Favorite page</div>
+        <div className="flex justify-center pt-10 mx-auto h-screen w-screen text-center mt-2 ">
+            <ul className="list-none">
+                {favorites.map(f => (
+                    <li key={f}>
+                        <a href={f} target="_blank">{f}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
